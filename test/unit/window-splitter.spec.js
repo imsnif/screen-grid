dbb354414c3b46171f7db14f459e91c579b2cc94
Aram Drevekenin <grimsniffer@gmail.com>
2016-11-19 12:22:30 +0200

    test: split current window vs. split window

--- test/unit/window-splitter.spec.js
+++ test/unit/window-splitter.spec.js
@@ -16,7 +16,7 @@ function stubWindowSplitterNoFocusedWin (pane) {
   })
 }
 
-test('splitCurrentWindow(screenId, winConstructor, winParams, axis): ' +
+test('splitWindow(screenId, winConstructor, winParams, axis, winId): ' +
      'splits current window (horizontally)', t => {
   t.plan(2)
   const changeSize = sinon.spy()
@@ -24,12 +24,13 @@ test('splitCurrentWindow(screenId, winConstructor, winParams, axis): ' +
   const pane = {id: 1, x: 0, y: 0, height: 100, width: 100, changeSize}
   const grid = 'I am a grid'
   const grids = [ grid ]
-  const { splitCurrentWindow } = stubWindowSplitter(pane)({grids, createWindow})
-  splitCurrentWindow(
+  const { splitWindow } = stubWindowSplitter(pane)({grids, createWindow})
+  splitWindow(
     1,
     {fakeWinConstructor: 'fakeWinConstructor'},
     {fakeWinParams: 'fakeWinParams'},
-    'horizontal'
+    'horizontal',
+    1
   )
   t.ok(changeSize.calledWith(100, 50), 'window resized to half size horizontally')
   t.ok(createWindow.calledWith(1, {fakeWinConstructor: 'fakeWinConstructor'}, {
@@ -41,7 +42,7 @@ test('splitCurrentWindow(screenId, winConstructor, winParams, axis): ' +
   }), 'new window created half the size of previous')
 })
 
-test('splitCurrentWindow(screenId, winConstructor, winParams, axis): ' +
+test('splitWindow(screenId, winConstructor, winParams, axis, winId): ' +
      'splits current window (vertically)', t => {
   t.plan(2)
   const changeSize = sinon.spy()
@@ -49,12 +50,13 @@ test('splitCurrentWindow(screenId, winConstructor, winParams, axis): ' +
   const pane = {id: 1, x: 0, y: 0, height: 100, width: 100, changeSize}
   const grid = 'I am a grid'
   const grids = [ grid ]
-  const { splitCurrentWindow } = stubWindowSplitter(pane)({grids, createWindow})
-  splitCurrentWindow(
+  const { splitWindow } = stubWindowSplitter(pane)({grids, createWindow})
+  splitWindow(
     1,
     {fakeWinConstructor: 'fakeWinConstructor'},
     {fakeWinParams: 'fakeWinParams'},
-    'vertical'
+    'vertical',
+    1
   )
   t.ok(changeSize.calledWith(50, 100), 'window resized to half size vertically')
   t.ok(createWindow.calledWith(1, {fakeWinConstructor: 'fakeWinConstructor'}, {
@@ -64,6 +66,48 @@ test('splitCurrentWindow(screenId, winConstructor, winParams, axis): ' +
     x: 50,
     y: 0
   }), 'new window created half the size of previous')
+})
+
+test('splitCurrentWindow(screenId, winConstructor, winParams, axis): ' +
+     'calls splitWindow with id of focused window', t => {
+  t.plan(1)
+  const splitWindow = sinon.spy()
+  const isInGrid = sinon.stub().returns(true)
+  const pane = {id: 1, x: 0, y: 0, height: 100, width: 100}
+  const grid = 'I am a grid'
+  const grids = [ grid ]
+  const { splitCurrentWindow } = stubWindowSplitter(pane)({grids, splitWindow, isInGrid})
+  splitCurrentWindow(
+    1,
+    {fakeWinConstructor: 'fakeWinConstructor'},
+    {fakeWinParams: 'fakeWinParams'},
+    'horizontal'
+  )
+  t.ok(splitWindow.calledWith(
+    1,
+    {fakeWinConstructor: 'fakeWinConstructor'},
+    {fakeWinParams: 'fakeWinParams'},
+    'horizontal', 1),
+    'splitWindow called with focused window id'
+  )
+})
+
+test('splitCurrentWindow(screenId, winConstructor, winParams, axis): ' +
+     'does not call splitWindow if current pane is not in desired grid', t => {
+  t.plan(1)
+  const splitWindow = sinon.spy()
+  const isInGrid = sinon.stub().returns(false)
+  const pane = {id: 1, x: 0, y: 0, height: 100, width: 100}
+  const grid = 'I am a grid'
+  const grids = [ grid ]
+  const { splitCurrentWindow } = stubWindowSplitter(pane)({grids, splitWindow, isInGrid})
+  splitCurrentWindow(
+    1,
+    {fakeWinConstructor: 'fakeWinConstructor'},
+    {fakeWinParams: 'fakeWinParams'},
+    'horizontal'
+  )
+  t.ok(splitWindow.notCalled, 'no-op')
 })
 
 test('splitCurrentWindow(screenId, winConstructor, winParams, axis): ' +
