e27529b838a4f7e277474c8e73eb6c7b69c9acaf
Aram Drevekenin <grimsniffer@gmail.com>
2016-11-19 15:57:09 +0200

    test: add window centered

--- test/unit/window-creator.spec.js
+++ test/unit/window-creator.spec.js
@@ -75,3 +75,73 @@ test('createWindow(gridId, winConstructor, opts): ' +
     'window not added to second grid'
   )
 })
+
+test('createWindowCentered(gridId, winConstructor, opts): ' +
+     'adds window to specified grid at the center of the largest gap', t => {
+  t.plan(1)
+  const grids = [{
+    id: 1,
+    add: sinon.spy(),
+    findGaps: sinon.stub().returns([
+      { x: 100, y: 100, height: 500, width: 500 },
+      { x: 500, y: 500, height: 300, width: 300 }
+    ])},
+    {id: 2, add: sinon.spy()}
+  ]
+  const { createWindowCentered } = stubWindowCreator()({grids})
+  createWindowCentered(1, 'winConstructor', {x: 0, y: 0, width: 100, height: 100})
+  t.ok(grids[0].add.calledWith(
+    'winConstructor',
+    {x: 300, y: 300, width: 100, height: 100}
+  ), 'pane created in the center of largest gap')
+})
+
+test('createWindowCentered(gridId, winConstructor, opts): ' +
+     'no-op if failed to add window to grid', t => {
+  t.plan(1)
+  const grids = [
+    {id: 1, add: sinon.stub().throws()}
+  ]
+  const { createWindowCentered } = stubWindowCreator()({grids})
+  createWindowCentered(1, 'winConstructor', {x: 0, y: 0, width: 100, height: 100})
+  t.pass('no-op')
+})
+
+test('createWindowCentered(null, winConstructor, opts): ' +
+     'adds window to focused grid if no id provided', t => {
+  t.plan(1)
+  const grids = [
+    {id: 2, add: sinon.spy()},
+    {
+      id: 1,
+      add: sinon.spy(),
+      findGaps: sinon.stub().returns([
+        { x: 100, y: 100, height: 500, width: 500 },
+        { x: 500, y: 500, height: 300, width: 300 }
+      ])
+    }
+  ]
+  const { createWindowCentered } = stubWindowCreator()({grids})
+  createWindowCentered(null, 'winConstructor', {x: 0, y: 0, width: 100, height: 100})
+  t.ok(
+    grids[1].add.calledWith(
+    'winConstructor',
+    {x: 300, y: 300, width: 100, height: 100}
+  ), 'pane created in the center of largest gap')
+})
+
+test('createWindowCentered(gridId, winConstructor, opts): ' +
+     'no-op if no gaps found', t => {
+  t.plan(1)
+  const grids = [
+    {
+      id: 1,
+      add: sinon.spy(),
+      findGaps: sinon.stub().returns([])
+    },
+    {id: 2, add: sinon.spy()}
+  ]
+  const { createWindowCentered } = stubWindowCreator()({grids})
+  createWindowCentered(1, 'winConstructor', {x: 0, y: 0, width: 100, height: 100})
+  t.ok(grids[0].add.notCalled, 'no-op')
+})
