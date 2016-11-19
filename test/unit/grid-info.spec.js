693f944d985cd6b6dc332e1ff0aac5b7ac4c4717
Aram Drevekenin <grimsniffer@gmail.com>
2016-11-19 12:27:54 +0200

    test: win is in grid

--- test/unit/grid-info.spec.js
+++ test/unit/grid-info.spec.js
@@ -41,3 +41,23 @@ test('currentPanePosition(): returns false if no focused window', t => {
     'returns false when there is no focused window'
   )
 })
+
+test('isInGrid(winId, screenId): returns true if pane id is in grid on screenId', t => {
+  t.plan(1)
+  const grids = [
+    {id: 1, panes: [ {id: 1}, {id: 2} ]},
+    {id: 2, panes: [ {id: 3}, {id: 4} ]}
+  ]
+  const { isInGrid } = stubGridInfoNoFocusedWindow()({grids})
+  t.ok(isInGrid(1, 1), 'returns true if pane is in grid')
+})
+
+test('isInGrid(winId, screenId): returns false if pane id is not in grid on screenId', t => {
+  t.plan(1)
+  const grids = [
+    {id: 1, panes: [ {id: 1}, {id: 2} ]},
+    {id: 2, panes: [ {id: 3}, {id: 4} ]}
+  ]
+  const { isInGrid } = stubGridInfoNoFocusedWindow()({grids})
+  t.notOk(isInGrid(1, 2), 'returns false if pane is not in grid')
+})
